11516107cb4169278de61d7e34d0dc9538b121e1
Josh Trivedi <josh.trivedi@gmail.com>
2022-03-04 13:29:04 +0530

    removed comments

--- main.py
+++ main.py
@@ -12,13 +12,11 @@ cap = cv2.VideoCapture("example.mp4")
 detect_faces = FaceDetection(model_type=FaceDetectionModel.FRONT_CAMERA)
 while True:
     _, img = cap.read()
-    #print(img.shape)
     width = img.shape[1]
     height = img.shape[0]
-    # width_px = width*img.shape[2]
-    # height_px = height*img.shape[2]
     st = time.time()
     faces = detect_faces(img)
+    
     et = time.time()
     no_face_str = "No face detected"
     text_coords = (0, height-10)
@@ -26,17 +24,14 @@ while True:
     inference = "inference time = "+ str("{:.2f}".format((et-st)*1000))
     if len(faces):
         render_data = detections_to_render_data(faces, bounds_color=Colors.GREEN)
-        #print(render_data)
-        #print(type(render_data))
-        #print(type(render_data[0]))
         list_1 = list()
         for i in render_data:
             for j in i.data:
-                #print("left = " , j.left*width, " right = ", j.right*width, " top = ", j.top*height, " bottom = ", j.bottom*height)
+                
                 left_top = (int (j.left*width), int (j.top*height))
                 right_bottom = (int (j.right*width), int (j.bottom*height))
                 cv2.rectangle(img, left_top, right_bottom, red_color, 3)
                 cv2.putText(img, inference , text_coords, cv2.FONT_HERSHEY_SIMPLEX, 1, red_color, 2, cv2.LINE_AA)
                 cv2.imshow("FaceDetection",img)
                 cv2.waitKey(1)
-    
+
